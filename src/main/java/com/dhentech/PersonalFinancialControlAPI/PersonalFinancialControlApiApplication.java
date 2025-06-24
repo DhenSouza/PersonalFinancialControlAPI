7d14655acc0e353f49abfec60c31a923bab13aef
DhenSouza <denilson_contato@outlook.com>
2025-06-24 12:24:52 -0300

    feat: application initializer

---
+++ src/main/java/com/dhentech/PersonalFinancialControlAPI/PersonalFinancialControlApiApplication.java
@@ -0,0 +1,13 @@
+package com.dhentech.PersonalFinancialControlAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinancialControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinancialControlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhentech/PersonalFinancialControlAPI/PersonalFinancialControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhentech.PersonalFinancialControlAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinancialControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
